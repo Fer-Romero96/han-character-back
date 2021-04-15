3347bf30c91ddb090ed85047caa55e4f0b93e121
velirean <47877809+velirean@users.noreply.github.com>
2021-04-15 16:48:09 -0500

    Obtiene audio de pronunciación de kanji

--- hanchar/app/Http/Controllers/Api/CharactersController.php
+++ hanchar/app/Http/Controllers/Api/CharactersController.php
@@ -12,12 +12,13 @@ class CharactersController extends Controller
     public function info($char) {
         $url = 'https://kanjialive-api.p.rapidapi.com/api/public/kanji/';
         $url .= $char;
+        $url_audio = 'https://api.voicerss.org';
 
-        // $response = Http::dd()->withHeaders([
+        // Rapid API call, it gets most of the information
         $response = Http::withHeaders([
             'x-rapidapi-key' => env('RAPID_API_KEY'),
 	        'x-rapidapi-host' => 'kanjialive-api.p.rapidapi.com'
-        ])->get($url)->json();
+            ])->get($url)->json();
 
         if (array_key_exists('kanji', $response)) {
 
@@ -28,6 +29,34 @@ class CharactersController extends Controller
             $data['kanji']['kunyomi'] = $response['kanji']['kunyomi'];
             $data['radical'] = $response['radical'];
             $data['examples'] = $response['examples'];
+
+            // Request mp3 file for onyomi
+            if ($data['kanji']['onyomi']['romaji'] != 'n/a')
+            {
+                $mp3_onyomi = Http::asForm()->post($url_audio, [
+                    'key' => env('VOICERSS_API_KEY'),
+                    'hl' => 'ja-jp',
+                    'v' => 'Humi',
+                    'c' => 'mp3',
+                    'src' => $data['kanji']['onyomi']['romaji']
+                ]);
+
+                $data['kanji']['onyomi']['mp3_base64'] = base64_encode($mp3_onyomi);
+            }
+
+            // Request mp3 file for kunyomi
+            if ($data['kanji']['kunyomi']['romaji'] != 'n/a')
+            {
+                $mp3_kunyomi = Http::asForm()->post($url_audio, [
+                    'key' => env('VOICERSS_API_KEY'),
+                    'hl' => 'ja-jp',
+                    'v' => 'Humi',
+                    'c' => 'mp3',
+                    'src' => $data['kanji']['kunyomi']['romaji']
+                ]);
+
+                $data['kanji']['kunyomi']['mp3_base64'] = base64_encode($mp3_kunyomi);
+            }
 
             return response()->json($data);
         } else {
